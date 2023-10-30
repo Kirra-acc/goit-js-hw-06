ae03c294a637c6ee31c656858d6033d6b86e6bda
Kirra-Potiekhina <kirrapotehina@gmail.com>
2023-10-30 19:05:01 +0200

    add/feature 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const controls = document.querySelector("#controls");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  const boxesArr = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesArr.push(box);
+  }
+
+  boxesArr.forEach((box) => {
+    boxes.appendChild(box);
+  });
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+controls.querySelector("button[data-create]").addEventListener("click", () => {
+  createBoxes(parseInt(controls.querySelector("input").value));
+});
+
+controls.querySelector("button[data-destroy]").addEventListener("click", destroyBoxes);
